9812deecac8b9cea9213a8b703316b193012ea5b
GCZhang <zhang.guangchun@qq.com>
2016-11-01 23:14:46 +0800

    修改gatherv_2.cpp

--- mpi/MPI_Gatherv/gatherv_2.cpp
+++ mpi/MPI_Gatherv/gatherv_2.cpp
@@ -9,15 +9,46 @@
  * 作者：G.C. Zhang
  * 目的：对MPI_Gatherv and MPI_Type_struct进行测试
  * 描述：
+ *  struct { vector; } not work.
+ *  [注意struct中的内存对齐，4个整型（4 * 4 bytes）刚好和2个实型(2 * 8bytes)长度一致。编
+ *  译器不必再额外为mesh_flum分配内存。否则，还要注意mesh_flum的真实长度和自定义的MPI类
+ *  型的长度是否一致。]
+ *  事实上，不必担心内存对齐的问题，因为使用了已经offsetof()函数求得了mesh_flum中每一个值
+ *  再mesh_flum内部的偏移。并且再构造mpi_flum_struct_type时已经考虑了位移
+ *------------------------------------------------------------------------------------
+ * TIME : 2016-11-1
+ * DEAIL : 将mesh_flum中的实型增加为4个。
 */
 
-typedef struct mesh_flum_s {
+struct mesh_flum {
    int face;
    int i;
    int j;
    int k;
-   double flum;
-} mesh_flum;
+
+   // seems does not work;
+   // std::vector<double> flum;
+   double flum0;
+   double flum1;
+   double flum2;
+   double flum3;
+};
+
+
+void print_mesh_flum(int myrank, const std::vector<mesh_flum> &mesh_flum_vec){
+      std::cout << "CPU " << myrank << ": " << std::endl;
+   for (std::vector<mesh_flum>::const_iterator i = mesh_flum_vec.begin(); i != mesh_flum_vec.end(); ++i){
+      std::cout << i->face << " ";
+      std::cout << i->i << " ";
+      std::cout << i->j << " ";
+      std::cout << i->k << " ";
+      std::cout << i->flum0 << " ";
+      std::cout << i->flum1 << " ";
+      std::cout << i->flum2 << " ";
+      std::cout << i->flum3 << " ";
+      std::cout << std::endl;
+   }
+}
 
 int main(int argc, char *argv[])
 {
@@ -28,11 +59,6 @@ int main(int argc, char *argv[])
    
    int root, myrank, nProc;
    root = 0;
-   
-   std::vector<mesh_flum> v_send;
-   std::vector<mesh_flum> v_receive;
-   std::vector<int> v_counts;
-   std::vector<int> v_displs;
 
    // Get process numbers and my rank id.
    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -41,27 +67,50 @@ int main(int argc, char *argv[])
 //---------------------------------------------------
    /* create a type for struct mesh_flum. */
    const int nitms = 5;
-   int blocklengths[5] = {1,1,1,1,1};
-   MPI_Datatype types[5] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT, MPI_DOUBLE};
-   MPI_Datatype mpi_flum_type;
-   MPI_Aint offsets[5];
+   int LengdreNum = 4;
+
+   MPI_Datatype mpi_flum_vec_type;
+   MPI_Type_contiguous(LengdreNum, MPI_DOUBLE, &mpi_flum_vec_type);
+   MPI_Type_commit(&mpi_flum_vec_type);
+
+   int blocklengths[nitms] = {1,1,1,1,1};
+   MPI_Datatype types[nitms] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT, mpi_flum_vec_type};
+   
+   MPI_Datatype mpi_flum_struct_type;
+   MPI_Aint offsets[nitms];
 
    offsets[0] = offsetof(mesh_flum, face);
    offsets[1] = offsetof(mesh_flum, i);
    offsets[2] = offsetof(mesh_flum, j);
    offsets[3] = offsetof(mesh_flum, k);
-   offsets[4] = offsetof(mesh_flum, flum);
-   /*for (int i=0; i!=5; ++i) {cout << "offsets: " << offsets[i] << " ";}*/
+   offsets[4] = offsetof(mesh_flum, flum0);
+   if (myrank == 0){
+      cout << "offsets: " ;
+      for (int i=0; i!=nitms; ++i) {cout << offsets[i] << " ";}
+      cout << endl;
+   }
 
-   MPI_Type_struct(nitms, blocklengths, offsets, types, &mpi_flum_type);
-   MPI_Type_commit(&mpi_flum_type);   
+   MPI_Type_struct(nitms, blocklengths, offsets, types, &mpi_flum_struct_type);
+   MPI_Type_commit(&mpi_flum_struct_type);   
    
 //---------------------------------------------------
-   int send_size = myrank + 1;
-   v_send.resize(send_size);
+   /* 设置v_counts, v_displs和v_send */ 
+   std::vector<int> v_counts;
+   std::vector<int> v_displs;
 
    if (myrank == 0) v_counts.resize(nProc, 0);
    if (myrank == 0) v_displs.resize(nProc, 0);
+
+   // prepare v_send;
+   std::vector<mesh_flum> v_send;
+   for (int i = 0; i < myrank+1; ++i)
+   {
+      v_send.push_back(mesh_flum{myrank+1, myrank+1, myrank+1, myrank+1,
+                              (myrank+1)*0.1, (myrank+1)*0.1, (myrank+1)*0.1, (myrank+1)*0.1});
+   }
+
+   // get send size of each process.
+   int send_size = v_send.size();
 
    // Gather v_counts[] from all process.
    MPI_Gather(&send_size, 1, MPI_INT,
@@ -81,42 +130,26 @@ int main(int argc, char *argv[])
       v_displs[0] = 0;
       for (int i = 1; i != nProc; ++i){
          v_displs[i] = v_displs[i-1] + v_counts[i-1];
-         std::cout << "v_displs[" << i << "] is: " << v_displs[i] << std::endl; 
+         std::cout << "v_displs[" << i << "] is: " << v_displs[i] << std::endl;
       }
    }
-
+//---------------------------------------------------
+   
+   std::vector<mesh_flum> v_receive;
+   
    /* 只有0号CPU申请缓冲区的值 */
    if (myrank == root) v_receive.resize(sum);
 
-   /* 设置v_send的值*/
-   for (std::vector<mesh_flum>::iterator i = v_send.begin(); i != v_send.end(); ++i){
-      (*i).face = myrank + 1;
-      (*i).i = myrank + 1;
-      (*i).j = myrank + 1;
-      (*i).k = myrank + 1;
-      (*i).flum = myrank;
-   }
-
-//---------------------------------------------------
-
    // 搜集操作
-   MPI_Gatherv(&v_send[0], send_size, mpi_flum_type, 
+   MPI_Gatherv(&v_send[0], send_size, mpi_flum_struct_type, 
       &v_receive[0], &v_counts[0], &v_displs[0],
-      mpi_flum_type, root, MPI_COMM_WORLD);
+      mpi_flum_struct_type, root, MPI_COMM_WORLD);
 
    // 打印
    if (myrank == root){
-      for (std::vector<mesh_flum>::iterator i = v_receive.begin(); i != v_receive.end(); ++i){
-         std::cout << (*i).face << " ";
-         std::cout << (*i).i << " ";
-         std::cout << (*i).j << " ";
-         std::cout << (*i).k << " ";
-         std::cout << (*i).flum << " ";
-         std::cout << std::endl;
-      }
-      std::cout << std::endl;
+      print_mesh_flum(myrank, v_receive);
    }
-
+//---------------------------------------------------
    MPI_Finalize();
    return 0;
 }
